6fba2668140d569ef7f9042ef959a6446f4c9131
Aeron00 <98010026+Aeron00@users.noreply.github.com>
2022-02-01 11:35:32 +0530

    Add files via upload

---
+++ Practical_4_Salesman/salesman.py
@@ -0,0 +1,18 @@
+A = input("salesman name:-")
+B = input("salesman id:-")
+C = "Ahemdabad"
+D = 'Nadiad'
+E = int(input("Total sale today:-"))
+F = E*0.1
+print('******************************************************')
+print('Joining date:-1/1/2020          Current date:-1/1/2022')
+print('*****************TODAYS SALES REPORT******************')
+print(F'salesman name:-{A}                   salesman id:-{B}')
+print('******************************************************')
+print(F"Route :- From {C} to {D}")
+print('******************************************************')
+print(F'Total sales in a day:-{E}')
+print(F'Commision = {F}')
+print('******************************************************')
+
+
