ae09238b69d27be542a67bb023667e9f95f6cd0f
Aeron00 <98010026+Aeron00@users.noreply.github.com>
2022-02-01 20:30:47 +0530

    Add files via upload

---
+++ Practical_6_Newspaper_Distribution_System/newspaperdistributorsystem.py
@@ -0,0 +1,61 @@
+print('===============================================')
+DELIVERYBOY1NAME = input('ENTER DELIVERY BOY1 NAME:-')
+
+print('===============================================')
+print(F'Enter allocated total papers to {DELIVERYBOY1NAME}')
+DIVYABHASKAR1 = int(input('DIVYABHASKAR:-'))
+SANDESH1 = int(input('SANDESH:-'))
+ECONOMICS1 = int(input('ECONOMICS:-'))
+TIMESOFINDIA1 = int(input('TIMES OF INDIA:-'))
+SANJSAMACHAR1 = int(input('SANJSAMACHAR:-'))
+JANSATA1 = int(input('JANSATA:-'))
+print('===============================================')
+
+TOTALALLOCATEDPAPERS1 = DIVYABHASKAR1 + SANDESH1 + ECONOMICS1 + TIMESOFINDIA1 + SANJSAMACHAR1 + JANSATA1
+print(F'Total allocated papers ={TOTALALLOCATEDPAPERS1}')
+TOTALSOLDPAPERS1 = int(input('Enter total count of sold papers:-'))
+TOTALREMAININGPAPERS1 = TOTALALLOCATEDPAPERS1 - TOTALSOLDPAPERS1
+print(F'total count of remaining papers ={TOTALREMAININGPAPERS1}')
+print('===============================================')
+
+SALARY1 = int(input('Salary ='))
+COMMISION1 = TOTALSOLDPAPERS1 * 0.02 * 5
+print(F'Commision ={COMMISION1}')
+TOTALSALARY1 = SALARY1 + COMMISION1
+print(F'Total salary = {TOTALSALARY1}')
+print('===============================================')
+
+print('===============================================')
+DELIVERYBOY2NAME = input('ENTER DELIVERY BOY2 NAME:-')
+
+print('===============================================')
+print(F'Enter allocated total papers to {DELIVERYBOY2NAME}')
+DIVYABHASKAR2 = int(input('DIVYABHASKAR:-'))
+SANDESH2 = int(input('SANDESH:-'))
+ECONOMICS2 = int(input('ECONOMICS:-'))
+TIMESOFINDIA2 = int(input('TIMES OF INDIA:-'))
+SANJSAMACHAR2 = int(input('SANJSAMACHAR:-'))
+JANSATA2 = int(input('JANSATA:-'))
+print('===============================================')
+
+TOTALALLOCATEDPAPERS2 = DIVYABHASKAR2 + SANDESH2 + ECONOMICS2 + TIMESOFINDIA2 + SANJSAMACHAR2 + JANSATA2
+print(F'Total allocated papers ={TOTALALLOCATEDPAPERS2}')
+TOTALSOLDPAPERS2 = int(input('Enter total count of sold papers:-'))
+TOTALREMAININGPAPERS2 = TOTALALLOCATEDPAPERS2 - TOTALSOLDPAPERS2
+print(F'total count of remaining papers ={TOTALREMAININGPAPERS2}')
+print('===============================================')
+
+SALARY2 = int(input('Salary ='))
+COMMISION2 = TOTALSOLDPAPERS2 * 0.02 * 5
+print(F'Commision ={COMMISION2}')
+TOTALSALARY2 = SALARY2 + COMMISION2
+print(F'Total salary = {TOTALSALARY2}')
+print('===============================================')
+
+
+
+
+
+
+
+
